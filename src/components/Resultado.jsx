d6ebeb5ab2e74e1749ac212b75207827611fd98a
Edgardo803 <edgardo_148@hotmail.com>
2023-05-31 09:14:41 +0200

    Ultimo commit del proyecto

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,21 @@
+import {Fragment, useCallback, useMemo, useRef} from 'react'
 import useCotizador from "../hooks/useCotizador"
 import { MARCAS, PLANES } from '../constants'
 
 const  Resultado = () => {
     const {resultado, datos} = useCotizador()
     const {marca, plan, year} = datos
+    const yearRef = useRef(year)
 
-    console.log(MARCAS)
+    const [nombreMarca] = useMemo( () =>
+      MARCAS.filter(m => m.id === Number(marca) ), 
+      [resultado])
+    const [nombrePlan] = useMemo( () =>
+      PLANES.filter(p => p.id === Number(plan) ),
+      [resultado]
+    )
+
+    
 
     if(resultado === 0) return null
 
@@ -17,9 +27,26 @@ const  Resultado = () => {
 
         <p className="my-2">
             <span className="font-bold">Marca: </span>
-            {marca}
+            {nombreMarca.nombre}
 
         </p>
+
+        <p className="my-2">
+            <span className="font-bold">Plan: </span>
+            {nombrePlan.nombre}
+
+        </p>
+
+        <p className="my-2">
+            <span className="font-bold">Año del Auto: </span>
+            {yearRef.current}
+        </p>
+
+        <p className="my-2 text-2xl">
+            <span className="font-bold">Total Cotización: </span>
+            {resultado}
+        </p>
+
     </div>
   )
 }
